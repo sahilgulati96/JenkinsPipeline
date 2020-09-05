1c14add1dbf1c27c75f87ddb7a6ed6ef41cef1ad
Sahil Gulati <sahilgulati102@gmail.com>
2020-09-05 13:55:44 +0530

    Update mavenbuild.sh

--- jenkins/mavenbuild/mavenbuild.sh
+++ jenkins/mavenbuild/mavenbuild.sh
@@ -1,5 +1,5 @@
 echo "Starting Build of the maven App"
-docker run --rm -v $(pwd)/mavenapp:/app -v /root/.m2:/root/.m2 -w /app maven:alpine "$@"
+docker run --rm -v $(pwd)/JenkinsPipeline/mavenapp:/app -v /root/.m2:/root/.m2 -w /app maven:alpine "$@"
 
 echo "Copy the Jar file to build Directory"
 cp ./mavenapp/target/*jar ./

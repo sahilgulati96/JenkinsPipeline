bb32d1fc68cd143c68fd410771c45cf74fb705b4
Sahil <sahilgualti102@gmail.com>
2020-09-13 12:44:21 +0530

    Fixed Permission

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,6 +2,8 @@
 
 echo "sahilgulati102/mavenapp" > /tmp/.auth
 echo "$BUILD_TAG" >>/tmp/.auth
+
+chmod 400 aws.pem
 scp -i aws.pem /tmp/.auth ubuntu@3.81.6.80:/tmp/.auth
 jenkins/deploy
 scp -i aws.pem jenkins/deploy/publish.sh ubuntu@3.81.6.80:publish.sh

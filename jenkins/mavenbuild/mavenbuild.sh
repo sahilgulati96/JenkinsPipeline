e5eeaa4cc9bd77a167db5db462513729452f30bd
Sahil <sahilgualti102@gmail.com>
2020-09-13 12:26:52 +0530

    FIxed workspace

--- jenkins/mavenbuild/mavenbuild.sh
+++ jenkins/mavenbuild/mavenbuild.sh
@@ -2,7 +2,7 @@
 WORKSPACE=/home/sahil/jenkins_home/workspace/mavenproject
 
 echo "Starting Build of the maven App"
-docker run --rm -v $(WORKSPACE)/mavenapp:/app -v /root/.m2:/root/.m2 -w /app maven:alpine "$@"
+docker run --rm -v /home/sahil/jenkins_home/workspace/mavenproject/mavenapp:/app -v /root/.m2:/root/.m2 -w /app maven:alpine "$@"
 
 echo "Copy the Jar file to build Directory"
 cp ./mavenapp/target/*jar ./
--- jenkins/test/maventest.sh
+++ jenkins/test/maventest.sh
@@ -1,6 +1,6 @@
 WORKSPACE=/home/sahil/jenkins_home/workspace/mavenproject
 
 echo "Starting Tests for the maven App"
-docker run --rm -v $(WORKSPACE)/mavenapp:/app -v /root/.m2:/root/.m2 -w /app maven:alpine "$@"
+docker run --rm -v /home/sahil/jenkins_home/workspace/mavenproject/mavenapp:/app -v /root/.m2:/root/.m2 -w /app maven:alpine "$@"
 
 
